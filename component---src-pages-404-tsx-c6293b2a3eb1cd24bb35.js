d195b3fdb381aaeb03713875b57c15207064ed4d
yonda-yonda <minerva.yooda@gmail.com>
2022-10-18 20:48:29 +0900

    Updates

---
+++ component---src-pages-404-tsx-c6293b2a3eb1cd24bb35.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkol_processed_source=self.webpackChunkol_processed_source||[]).push([[218],{2513:function(e,n,t){t.r(n);var l=t(7294),r=t(2265),c=t(7119),u=t(3854);n.default=function(){return l.createElement(l.Fragment,null,l.createElement(r.ZP,null),l.createElement(c.Z,null,l.createElement(u.Z,{variant:"h1",component:"h1"},"404")))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-c6293b2a3eb1cd24bb35.js.map
